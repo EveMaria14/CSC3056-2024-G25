eef4e5cb8bd67940f20cc796aa2472c415dee468
EveMaria14 <94977814+EveMaria14@users.noreply.github.com>
2024-03-02 18:05:12 +0000

    Update RangeTest.java
    Fixing some test errors, adding a couple I missed, updating  comments and names

--- Lab2/src/org/jfree/data/test/RangeTest.java
+++ Lab2/src/org/jfree/data/test/RangeTest.java
@@ -27,6 +27,233 @@ public class RangeTest {
 		
 	}
 	
+	// Test methods for intersects(double lower, double upper)
+	
+	// with positives (5, 10)
+	@Test
+	public void testIntersectsBothLeftOfRangeReturnsFalse() { 
+		assertFalse("Should return false", rangeObjectUnderTest.intersects(0, 3));
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftOfRangeUpperLeftBoundaryReturnsTrue() { 
+		// Boundary Assumption
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(1, 5));
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftOfRangeUpperInRangeReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(1, 7));
+	}
+
+	@Test
+	public void testIntersectsLowerLeftOfRangeUpperRightBoundaryReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(1, 10));
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftOfRangeUpperRightOfRangeReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(1, 12));
+	}
+	
+	@Test
+	public void testIntersectsBothLowerBoundaryReturnsTrue() { 
+		// Boundary Assumption
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(5, 5));
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftBoundaryUpperInRangeReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(5, 7));
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftBoundaryUpperRightBoundaryReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(5, 10));
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftBoundaryUpperRightOfRangeReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(5, 13));
+	}
+	
+	@Test
+	public void testIntersectsBothInRangeReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(7, 9));
+	}
+	
+	@Test
+	public void testIntersectsLowerInRangeUpperRightBoundaryReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(7, 10));
+	}
+	
+	@Test
+	public void testIntersectsLowerInRangeUpperRightOfRangeReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(7, 13));
+	}
+	
+	@Test
+	public void testIntersectsBothRightBoundaryReturnsTrue() { 
+		// Boundary Assumption
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(10, 10));
+	}
+	
+	@Test
+	public void testIntersectsLowerRightBoundaryUpperRightOfRangeReturnsTrue() { 
+		// Boundary Assumption
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(10, 14));
+	}
+	
+	@Test
+	public void testIntersectsBothRightOfRangeReturnsFalse() { 
+		assertFalse("Should return false", rangeObjectUnderTest.intersects(13, 15));
+	}
+	
+	// with negatives (-10, -5)
+	@Test
+	public void testIntersectsBothLeftOfRangeNegativesReturnsFalse() { 
+		assertFalse("Should return false", negativeRangeObjectUnderTest.intersects(-15, -13));
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftOfRangeUpperInRangeNegativesReturnsTrue() { 
+		assertTrue("Should return true", negativeRangeObjectUnderTest.intersects(-15, -7));
+	}
+	
+	@Test
+	public void testIntersectsBothInRangeNegativesReturnsTrue() { 
+		assertTrue("Should return true", negativeRangeObjectUnderTest.intersects(-9, -6));
+	}
+
+	@Test
+	public void testIntersectsLowerInRangeUpperRightOfRangeNegativesReturnsTrue() { 
+		assertTrue("Should return true", negativeRangeObjectUnderTest.intersects(-9, -3));
+	}
+	
+	@Test
+	public void testIntersectsBothRightOfRangeNegativesReturnsFalse() { 
+		assertFalse("Should return false", negativeRangeObjectUnderTest.intersects(-4, -2));
+	}
+		
+	
+	// Test methods for getLowerBound()
+	
+	@Test
+	public void testLowerBoundTwoNegatives() { 
+		customRangeObjectUnderTest = new Range(-5,-1);
+		assertEquals("The lower bound should be -5",
+				-5, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundOneNegativeOneZero() { 
+		customRangeObjectUnderTest = new Range(-5,0);
+		assertEquals("The lower bound should be -5",
+				-5, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundOneNegativeOnePositive() { 
+		customRangeObjectUnderTest = new Range(-5,1);
+		assertEquals("The lower bound should be -5",
+				-5, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundTwoZeros() { 
+		customRangeObjectUnderTest = new Range(0,0);
+		assertEquals("The lower bound should be 0",
+				0, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundOneZeroOnePositive() { 
+		customRangeObjectUnderTest = new Range(0,5);
+		assertEquals("The lower bound should be 0",
+				0, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundTwoPositives() { 
+		customRangeObjectUnderTest = new Range(5,15);
+		assertEquals("The lower bound should be 5",
+				5, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundEqualPositives() { 
+		customRangeObjectUnderTest = new Range(5,5);
+		assertEquals("The lower bound should be 5",
+				5, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundEqualNegatives() { 
+		customRangeObjectUnderTest = new Range(-5,-5);
+		assertEquals("The lower bound should be -5",
+				-5, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	
+	// Test methods for getUpperBound()
+	
+	@Test
+	public void testUpperBoundTwoNegatives() { 
+		customRangeObjectUnderTest = new Range(-5,-1);
+		assertEquals("The upper bound should be -1",
+				-1, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundOneNegativeOneZero() { 
+		customRangeObjectUnderTest = new Range(-5,0);
+		assertEquals("The upper bound should be 0",
+				0, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundOneNegativeOnePositive() { 
+		customRangeObjectUnderTest = new Range(-5,1);
+		assertEquals("The upper bound should be 1",
+				1, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundTwoZeros() { 
+		customRangeObjectUnderTest = new Range(0,0);
+		assertEquals("The upper bound should be 0",
+				0, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundOneZeroOnePositive() { 
+		customRangeObjectUnderTest = new Range(0,5);
+		assertEquals("The upper bound should be 5",
+				5, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundTwoPositives() { 
+		customRangeObjectUnderTest = new Range(5,15);
+		assertEquals("The upper bound should be 15",
+				15, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundEqualPositives() { 
+		customRangeObjectUnderTest = new Range(5,5);
+		assertEquals("The upper bound should be 5",
+				5, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundEqualNegatives() { 
+		customRangeObjectUnderTest = new Range(-5,-5);
+		assertEquals("The upper bound should be -5",
+				-5, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	
 	// Test methods for getCentralValues()
 	
 	@Test
@@ -41,13 +268,6 @@ public class RangeTest {
 		customRangeObjectUnderTest = new Range(3,3);
 		assertEquals("The central value of 3 and 3 should be 3",
 				3, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
-	}
-	
-	@Test
-	public void testCentralValueHalfValue() { 
-		customRangeObjectUnderTest = new Range(3,4);
-		assertEquals("The central value of 3 and 4 should be 3.5",
-				3.5, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
 	}
 	
 	@Test
@@ -79,14 +299,29 @@ public class RangeTest {
 	}
 	
 	@Test
+	public void testCentralValueBothZero() { 
+		customRangeObjectUnderTest = new Range(0,0);
+		assertEquals("The central value of 0 and 0 should be 0",
+				0, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+	
+	@Test
 	public void testCentralValueOneZeroOnePositive() { 
 		customRangeObjectUnderTest = new Range(0,5);
 		assertEquals("The central value of 0 and 5 should be 2.5",
 				2.5, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
 	}
 	
+	@Test
+	public void testCentralValueEqualNegativeValues() { 
+		customRangeObjectUnderTest = new Range(-3,-3);
+		assertEquals("The central value of -3 and -3 should be -3",
+				-3, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+	
 	
 	// Test methods for constrain(double value)
+	
 	// Using (5, 10) by default
 	@Test
 	public void testConstrainMatchesLowerRange() { 
@@ -123,233 +358,30 @@ public class RangeTest {
 	@Test
 	public void testConstrainMatchesLowerRangeNegative() { 
 		assertEquals("The constrain value should be -10",
-				-10, rangeObjectUnderTest.constrain(-11), 0.000000001d);
+				-10, negativeRangeObjectUnderTest.constrain(-11), 0.000000001d);
 	}
 	
 	@Test
 	public void testConstrainMatchesUpperRangeNegative() { 
 		assertEquals("The constrain value should be -5",
-				-5, rangeObjectUnderTest.constrain(-4), 0.000000001d);
+				-5, negativeRangeObjectUnderTest.constrain(-4), 0.000000001d);
 	}
 	
 	@Test
 	public void testConstrainWithinRangeNegative() { 
 		assertEquals("Should return input value (-7)",
-				-7, rangeObjectUnderTest.constrain(-7), 0.000000001d);
+				-7, negativeRangeObjectUnderTest.constrain(-7), 0.000000001d);
 	}
 	
 	@Test
 	public void testConstrainNegativeLowerBoundary() { 
 		assertEquals("The constrain value should be -10",
-				-10, rangeObjectUnderTest.constrain(-10), 0.000000001d);
+				-10, negativeRangeObjectUnderTest.constrain(-10), 0.000000001d);
 	}
 	
 	@Test
 	public void testConstrainNegativeUpperBoundary() { 
 		assertEquals("The constrain value should be -5",
-				-5, rangeObjectUnderTest.constrain(-5), 0.000000001d);
+				-5, negativeRangeObjectUnderTest.constrain(-5), 0.000000001d);
 	}
-	
-	// Test methods for getLowerBound()
-	
-	@Test
-	public void testLowerBoundTwoNegatives() { 
-		customRangeObjectUnderTest = new Range(-5,-1);
-		assertEquals("The lower bound should be -5",
-				-5, rangeObjectUnderTest.getLowerBound(), 0.000000001d);
-	}
-	
-	@Test
-	public void testLowerBoundOneNegativeOneZero() { 
-		customRangeObjectUnderTest = new Range(-5,0);
-		assertEquals("The lower bound should be -5",
-				-5, rangeObjectUnderTest.getLowerBound(), 0.000000001d);
-	}
-	
-	@Test
-	public void testLowerBoundOneNegativeOnePositive() { 
-		customRangeObjectUnderTest = new Range(-5,1);
-		assertEquals("The lower bound should be -5",
-				-5, rangeObjectUnderTest.getLowerBound(), 0.000000001d);
-	}
-	
-	@Test
-	public void testLowerBoundTwoZeros() { 
-		customRangeObjectUnderTest = new Range(0,0);
-		assertEquals("The lower bound should be 0",
-				0, rangeObjectUnderTest.getLowerBound(), 0.000000001d);
-	}
-	
-	@Test
-	public void testLowerBoundOneZeroOnePositive() { 
-		customRangeObjectUnderTest = new Range(0,5);
-		assertEquals("The lower bound should be 0",
-				0, rangeObjectUnderTest.getLowerBound(), 0.000000001d);
-	}
-	
-	@Test
-	public void testLowerBoundTwoPositives() { 
-		customRangeObjectUnderTest = new Range(5,15);
-		assertEquals("The lower bound should be 5",
-				5, rangeObjectUnderTest.getLowerBound(), 0.000000001d);
-	}
-	
-	// Test methods for getUpperBound()
-	
-	@Test
-	public void testUpperBoundTwoNegatives() { 
-		customRangeObjectUnderTest = new Range(-5,-1);
-		assertEquals("The lower bound should be -1",
-				-1, rangeObjectUnderTest.getUpperBound(), 0.000000001d);
-	}
-	
-	@Test
-	public void testUpperBoundOneNegativeOneZero() { 
-		customRangeObjectUnderTest = new Range(-5,0);
-		assertEquals("The lower bound should be 0",
-				0, rangeObjectUnderTest.getUpperBound(), 0.000000001d);
-	}
-	
-	@Test
-	public void testUpperBoundOneNegativeOnePositive() { 
-		customRangeObjectUnderTest = new Range(-5,1);
-		assertEquals("The lower bound should be 1",
-				1, rangeObjectUnderTest.getUpperBound(), 0.000000001d);
-	}
-	
-	@Test
-	public void testUpperBoundTwoZeros() { 
-		customRangeObjectUnderTest = new Range(0,0);
-		assertEquals("The lower bound should be 0",
-				0, rangeObjectUnderTest.getUpperBound(), 0.000000001d);
-	}
-	
-	@Test
-	public void testUpperBoundOneZeroOnePositive() { 
-		customRangeObjectUnderTest = new Range(0,5);
-		assertEquals("The lower bound should be 5",
-				5, rangeObjectUnderTest.getUpperBound(), 0.000000001d);
-	}
-	
-	@Test
-	public void testUpperBoundTwoPositives() { 
-		customRangeObjectUnderTest = new Range(5,15);
-		assertEquals("The lower bound should be 15",
-				15, rangeObjectUnderTest.getUpperBound(), 0.000000001d);
-	}
-	
-	// Test methods for intersects(double lower, double upper)
-	
-	// with positives (5, 10)
-	@Test
-	public void testIntersectsBothLeftOfRange() { 
-		assertFalse("Should return false", rangeObjectUnderTest.intersects(0, 3));
-	}
-	
-	@Test
-	public void testIntersectsLowerLeftOfRangeUpperLeftBoundary() { 
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(1, 5));
-	}
-	
-	@Test
-	public void testIntersectsLowerLeftOfRangeUpperInRange() { 
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(1, 7));
-	}
-
-	@Test
-	public void testIntersectsLowerLeftOfRangeUpperRightBoundary() { 
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(1, 10));
-	}
-	
-	@Test
-	public void testIntersectsLowerLeftOfRangeUpperRightOfRange() { 
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(1, 12));
-	}
-	
-	@Test
-	public void testIntersectsBothLowerBoundary() { 
-		// Assumption
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(5, 5));
-	}
-	
-	@Test
-	public void testIntersectsLowerLeftBoundaryUpperInRange() { 
-		// Assumption
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(5, 7));
-	}
-	
-	@Test
-	public void testIntersectsLowerLeftBoundaryUpperRightBoundary() { 
-		// Assumption
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(5, 10));
-	}
-	
-	@Test
-	public void testIntersectsLowerLeftBoundaryUpperRightOfRange() { 
-		// Assumption
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(5, 13));
-	}
-	
-	@Test
-	public void testIntersectsBothInRange() { 
-		// Assumption
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(7, 9));
-	}
-	
-	@Test
-	public void testIntersectsLowerInRangeUpperRightBoundary() { 
-		// Assumption
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(7, 10));
-	}
-	
-	@Test
-	public void testIntersectsLowerInRangeUpperRightOfRange() { 
-		// Assumption
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(7, 13));
-	}
-	
-	@Test
-	public void testIntersectsBothRightBoundary() { 
-		// Assumption
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(10, 10));
-	}
-	
-	@Test
-	public void testIntersectsLowerRightBoundaryUpperRightOfRange() { 
-		// Assumption
-		assertTrue("Should return true", rangeObjectUnderTest.intersects(10, 14));
-	}
-	
-	@Test
-	public void testIntersectsBothRightOfRange() { 
-		// Assumption
-		assertFalse("Should return false", rangeObjectUnderTest.intersects(13, 15));
-	}
-	
-	// with negatives (-10, -5)
-	@Test
-	public void testIntersectsBothLeftOfRangeNegatives() { 
-		assertFalse("Should return false", negativeRangeObjectUnderTest.intersects(-15, -13));
-	}
-	
-	@Test
-	public void testIntersectsLowerLeftOfRangeUpperInRangeNegatives() { 
-		assertTrue("Should return true", negativeRangeObjectUnderTest.intersects(-15, -7));
-	}
-	
-	@Test
-	public void testIntersectsBothInRangeNegatives() { 
-		assertTrue("Should return true", negativeRangeObjectUnderTest.intersects(-9, -6));
-	}
-
-	@Test
-	public void testIntersectsLowerInRangeUpperRightOfRangeNegatives() { 
-		assertTrue("Should return true", negativeRangeObjectUnderTest.intersects(-9, -3));
-	}
-	
-	@Test
-	public void testIntersectsBothRightOfRangeNegatives() { 
-		assertFalse("Should return false", negativeRangeObjectUnderTest.intersects(-4, -2));
-	}
-	
 }
