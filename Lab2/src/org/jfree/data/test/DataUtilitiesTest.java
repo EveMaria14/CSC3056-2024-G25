80cacb3180d909cc18c06e1a3f60156524e7b244
Eve Wilson <evewilson@Eves-MacBook-Pro.local>
2024-03-16 15:11:34 +0000

    trying to get local branch up to date with remote branch

--- Lab2/src/org/jfree/data/test/DataUtilitiesTest.java
+++ Lab2/src/org/jfree/data/test/DataUtilitiesTest.java
@@ -126,6 +126,7 @@ public class DataUtilitiesTest {
 	    invalidValues.addValue(3, true, 1);
 	    invalidValues.addValue(1, 2, false);
 	    invalidValues.addValue(0, false, false);
+<<<<<<< HEAD
 
 	    try {
 	        DataUtilities.calculateColumnTotal(invalidValues, 108);
@@ -569,4 +570,448 @@ public class DataUtilitiesTest {
 	  
 	 
 }
+=======
+>>>>>>> 6ad9728 (updating old files)
 
+	    try {
+	        DataUtilities.calculateColumnTotal(invalidValues, 108);
+			fail("The expected outcome is IllegalArgumentException");
+		} catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", 
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	
+	
+	
+	//test method2 calculateRowTotal(Values2D data, row)
+	
+	@Test //expected 1.0 in row index 0
+	public void testValidDataAndValidRowTotal() {
+	    double result = DataUtilities.calculateRowTotal(values2D, 0);
+	    assertEquals("Incorrect. It should be 1.0", 1.0, result, 0.0000001d);
+	}
+	
+	@Test
+	public void testValidDataAndNegativeRowTotal() {
+	    double result = DataUtilities.calculateRowTotal(values2D, -2);
+	    assertEquals("Expected 0.0 for invalid row index", 0.0, result, 0.0000001d);
+	}
+	
+	@Test
+	public void testValidDataAndOutofRangeRightRowTotal() {
+	    double result = DataUtilities.calculateRowTotal(values2D, 102);
+	    assertEquals("Expected 0.0 for invalid row index", 0.0, result, 0.0000001d);
+	}
+	
+	@Test //expected IllegalArgumentException with null data and valid row
+	public void testNullDataAndValidRowTotal() {
+		try {
+			DataUtilities.calculateRowTotal(null, 0);
+			fail("The expected outcome is IllegalArgumentException");
+		} 
+		catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", 
+					e.getClass().equals(IllegalArgumentException.class));		}
+	}
+	
+	@Test //expected IllegalArgumentException with null data and negative row
+	public void testNullDataAndNegativeRowTotal() {
+		try {
+			DataUtilities.calculateRowTotal(null, -100);
+			fail("The expected outcome is IllegalArgumentException");
+		} catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", 
+					e.getClass().equals(IllegalArgumentException.class));		}
+	}
+	
+	@Test //expected IllegalArgumentException with null data and out of range (right) row
+	public void testNullDataAndOutofRangeRightRowTotal() {
+		try {
+			DataUtilities.calculateRowTotal(null, 1000);
+			fail("The expected outcome is IllegalArgumentException");
+		} catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", 
+					e.getClass().equals(IllegalArgumentException.class));		}
+	}
+	
+	@Test //expected IllegalArgumentException with invalid string data and valid row index
+	public void testInvalidStringDataAndValidRowTotal() {
+		DefaultKeyedValues2D invalidValues = new DefaultKeyedValues2D();
+		//invalid data
+		invalidValues.addValue(1, 0, 3); 
+	    invalidValues.addValue(1, 3, "Hi");
+	    invalidValues.addValue(0, "Hello", "Hi");
+
+	    try {
+	        DataUtilities.calculateRowTotal(invalidValues, 1);
+			fail("The expected outcome is IllegalArgumentException");
+		} catch (Exception e) {
+			assertTrue("Incorrect exception type thrown", 
+					e.getClass().equals(IllegalArgumentException.class));
+		}
+	}
+	
+	@Test //expected IllegalArgumentException with invalid non-2D data and negative row index
+	public void testInvalidNon2dDataAndNegativeRowTotal() {
+		DefaultKeyedValues2D invalidValues = new DefaultKeyedValues2D();
+		//invalid data (non-2D)
+		invalidValues.addValue(1, 0, 3); 
+		
+		 try {
+		        DataUtilities.calculateRowTotal(invalidValues, -2);
+				fail("The expected outcome is IllegalArgumentException");
+			} catch (Exception e) {
+				assertTrue("Incorrect exception type thrown", 
+						e.getClass().equals(IllegalArgumentException.class));
+			}
+	}
+	
+	@Test //expected IllegalArgumentException with invalid boolean data and out of range (right) row index
+	public void testInvalidBooleanAndOutofRangeRightRowTotal() {
+		DefaultKeyedValues2D invalidValues = new DefaultKeyedValues2D();
+		//invalid data
+		invalidValues.addValue(1, true, false); 
+		invalidValues.addValue(2, 2, 3); 
+		invalidValues.addValue(4, 5, -10); 
+
+		
+		 try {
+		        DataUtilities.calculateRowTotal(invalidValues, 10);
+				fail("The expected outcome is IllegalArgumentException");
+			} catch (Exception e) {
+				assertTrue("Incorrect exception type thrown", 
+						e.getClass().equals(IllegalArgumentException.class));
+			}
+	}
+	
+	
+	
+	//test method3 createNumberArray(double[] data)
+	
+	 @Test // Expected: {1.0, 2.0, 3.5, 10.0, -4.0}
+	 public void testValidDataCreateNumberArray() {
+		 double[] data = {1.0, 2.0, 3.5, 10.0, -4.0};
+	     Number[] result = DataUtilities.createNumberArray(data);
+
+	     Number[] expected = {1.0, 2.0, 3.5, 10.0, -4.0};
+	     assertArrayEquals(expected, result);
+	 }
+	 
+	 @Test //Expected: {4.9E-324,4.9E-324,4.9E-324}
+	 public void testMininumValueDataCreateNumberArray() {
+	     double[] data = {Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE};
+	     Number[] result = DataUtilities.createNumberArray(data);
+
+	     Number[] expected = {Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE};
+	     assertArrayEquals(expected, result);
+	 }
+	 
+	 @Test //Expected: {1.7976931348623157E308, 1.7976931348623157E308}
+	 public void testMaximumValueDataCreateNumberArray() {
+	     double[] data = {Double.MAX_VALUE, Double.MAX_VALUE};
+	     Number[] result = DataUtilities.createNumberArray(data);
+
+	     Number[] expected = {Double.MAX_VALUE, Double.MAX_VALUE};
+	     assertArrayEquals(expected, result);
+	 }
+	 
+	 @Test //Expected: {-1.7976931348623157E308, -1.7976931348623157E308}
+	 public void testOutsideRangeLeftDataCreateNumberArray() {
+	     double[] data = {-Double.MAX_VALUE, -Double.MAX_VALUE};
+	     Number[] result = DataUtilities.createNumberArray(data);
+
+	     Number[] expected = {-Double.MAX_VALUE, -Double.MAX_VALUE};
+	     assertArrayEquals(expected, result);
+	 }
+	 
+	 @Test //Expected: {}
+	 public void testOutsideRangeRightDataCreateNumberArray() {
+	     double[] infinityData = {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY};
+	     Number[] result = DataUtilities.createNumberArray(infinityData);
+
+	     Number[] expected = {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY};
+	     assertArrayEquals(expected, result);
+	 }
+	 
+	 @Test //Expected: IllegalArgumentException
+	 public void testNullDataCreateNumberArray() {
+		 double[] data = null;
+
+	     try {
+	    	 Number[] result = DataUtilities.createNumberArray(data);
+	         fail("Expected IllegalArgumentException");
+	     } catch (IllegalArgumentException e) {
+	    	 // This is the expected behavior, so the test passes.
+	     }
+	 }
+	 
+	 @Test //Expected: {}
+	 public void testEmptyDataCreateNumberArray() {
+		 double[] data = {}; //empty data 
+	     Number[] result = DataUtilities.createNumberArray(data);
+
+		 Number[] expected = {};
+	     assertArrayEquals(expected, result);
+	 }
+	 
+	 @Test //Expected: IllegalArgumentException
+	 public void testNonDoubleDataCreateNumberArray() {
+		 double[] data = {1, -3, -4, 4};  //invalid non-double values
+
+	     try {
+	    	 Number[] result = DataUtilities.createNumberArray(data);
+	         fail("Expected IllegalArgumentException");
+	     } catch (IllegalArgumentException e) {
+	    	 // This is the expected behavior, so the test passes.
+	     }
+	 }
+	 
+	 
+	 
+	 
+	 //test method4 createNumberArray2D(double[][] data)
+	 
+	 @Test //expected the same as validData
+	 public void testValidDataCreateNumberArray2D() {
+		 double[][] validData = {
+				 {1.0, 2.0, 3.0},
+				 {4.0, 5.0, 6.0},
+				 {7.0, 8.0, 9.0}
+				 };
+
+	     Number[][] result = DataUtilities.createNumberArray2D(validData);
+
+	     Number[][] expected = {
+	            {1.0, 2.0, 3.0},
+	            {4.0, 5.0, 6.0},
+	            {7.0, 8.0, 9.0}
+	        	};
+
+	     assertArrayEquals(expected, result);
+	 }
+	 
+	 @Test //Expected: {{4.9E-324,4.9E-324,4.9E-324},{4.9E-324,4.9E-324,4.9E-324},{4.9E-324,4.9E-324,4.9E-324}}
+	 public void testMininumValueDataCreateNumberArray2D() {
+	     double[][] data = {
+	    		 {Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE},
+	    		 {Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE},
+	    		 {Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE}
+	     		 };
+	     
+	     Number[][] result = DataUtilities.createNumberArray2D(data);
+
+	     Number[][] expected = {
+	    		 {Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE},
+	    		 {Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE},
+	    		 {Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE}
+	     		 };
+	     
+	     assertArrayEquals(expected, result);
+	 }
+	 
+	 @Test //Expected: {{1.7976931348623157E308, 1.7976931348623157E308},{1.7976931348623157E308, 1.7976931348623157E308}}
+	 public void testMaximumValueDataCreateNumberArray2D() {
+	     double[][] maxData = {
+	    		 {Double.MAX_VALUE, Double.MAX_VALUE},
+	    		 {Double.MAX_VALUE, Double.MAX_VALUE}
+	     		 };
+	     
+	     Number[][] result = DataUtilities.createNumberArray2D(maxData);
+
+	     Number[][] expected = {
+	    		 {Double.MAX_VALUE, Double.MAX_VALUE},
+	    		 {Double.MAX_VALUE, Double.MAX_VALUE}
+	     		 };
+	     
+	     assertArrayEquals(expected, result);
+	 }
+	 
+	 @Test //Expected: {{-1.7976931348623157E308, -1.7976931348623157E308}, {-1.7976931348623157E308, -1.7976931348623157E308}}
+	 public void testOutsideRangeLeftDataCreateNumberArray2D() {
+	     double[][] data = {
+	    		 {-Double.MAX_VALUE, -Double.MAX_VALUE},
+	    		 {-Double.MAX_VALUE, -Double.MAX_VALUE}
+	     		 };
+	     
+	     Number[][] result = DataUtilities.createNumberArray2D(data);
+
+	     Number[][] expected = {
+	    		 {-Double.MAX_VALUE, -Double.MAX_VALUE},
+	    		 {-Double.MAX_VALUE, -Double.MAX_VALUE}
+	     		 };
+	   
+	     assertArrayEquals(expected, result);
+	 }
+	 
+	 @Test //Expected: {{infinity, infinity}, {infinity, infinity}}
+	 public void testOutsideRangeRightDataCreateNumberArray2D() {
+	     double[][] infinityData = {
+	    		 {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
+	    		 {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY}
+	     		 };
+	     
+	     Number[][] result = DataUtilities.createNumberArray2D(infinityData);
+
+	     Number[][] expected = {
+	    		 {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
+	    		 {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY}
+	     		 };
+	   
+	     assertArrayEquals(expected, result);
+	 } 
+	 
+	 @Test //Expected: {{},{}}
+	 public void testEmptyDataCreateNumberArray2D() {
+		 double[][] emptyData = {{},{}}; //empty data 
+	     Number[][] result = DataUtilities.createNumberArray2D(emptyData);
+
+		 Number[][] expected = {{},{}};
+	     assertArrayEquals(expected, result);
+	 }
+	 
+	 @Test //Expected: IllegalArgumentException
+	 public void testNullDataCreateNumberArray2D() {
+		 double[][] data = null;
+
+	     try {
+	    	 Number[][] result = DataUtilities.createNumberArray2D(data);
+	         fail("Expected IllegalArgumentException");
+	     } catch (IllegalArgumentException e) {
+	    	 // This is the expected behavior, so the test passes.
+	     }
+	 }
+	 
+	 @Test //Expected: IllegalArgumentException
+	 public void testNon2dDataCreateNumberArray2D() {
+		 double[][] non2dData = {
+				 {1.0, 2.0, 3.0},
+				 {1.0, 2.0},
+				 {1.0}
+		 		 };
+		 
+		 try {
+			 DataUtilities.createNumberArray2D(non2dData);
+		     fail("Expected IllegalArgumentException");
+		 } catch (IllegalArgumentException e) {
+			 // This is the expected behavior, so the test passes.
+		 }
+	 }
+	 
+	 @Test //Expected: IllegalArgumentException
+	 public void testNonDoubleDataCreateNumberArray2D() {
+		 double[][] nonDoubleData = {
+				 {1, 2, 3, 4},
+				 {5, 6, 7, 8},
+				 {9, 10, 11, 12}
+		 		 };
+
+	     try {
+	    	 Number[][] result = DataUtilities.createNumberArray2D(nonDoubleData);
+	         fail("Expected IllegalArgumentException");
+	     } catch (IllegalArgumentException e) {
+	    	 // This is the expected behavior, so the test passes.
+	     }
+	 }
+	 
+	 
+	 
+	 
+	 //test method5 getCumulativePercentages(KeyedValues data)
+	 @Test //expected: 1.0
+	 public void testValidCumulativePercentages() {
+		 DefaultKeyedValues keyvalues = new DefaultKeyedValues();
+		 keyvalues.addValue((Comparable) 0.0, 6.0);
+		 keyvalues.addValue((Comparable) 1.0, 11.0);
+		 keyvalues.addValue((Comparable) 2.0, 3.0);
+		 KeyedValues object_under_test = DataUtilities.getCumulativePercentages((KeyedValues) keyvalues);
+		 
+		 assertEquals(1.0, (double) object_under_test.getValue(2), 0.000000001d);
+	 }
+	 
+	 @Test //expected NaN
+	 public void testMinimumValueCumulativePercentages() {
+		 DefaultKeyedValues keyvalues = new DefaultKeyedValues();
+		 keyvalues.addValue((Comparable) 0.0, 0.0);
+		 keyvalues.addValue((Comparable) 1.0, 0.0);
+		 keyvalues.addValue((Comparable) 2.0, 0.0);
+		 KeyedValues object_under_test = DataUtilities.getCumulativePercentages((KeyedValues) keyvalues);
+		 
+		 double result = (double) object_under_test.getValue(2);
+		 
+		 if(Double.isNaN(result)) {
+			 assertTrue(true); //pass the test, since 0 / 0 is undefined NaN
+		 } else {
+			 assertEquals(0.0, result, 0.000000001d);
+		 }
+	 }
+	 
+	 @Test //expected 0.0
+	 public void testNegativeDataCumulativePercentages() {
+		 DefaultKeyedValues keyvalues = new DefaultKeyedValues();
+		 keyvalues.addValue((Comparable) 0.0, -10);
+		 keyvalues.addValue((Comparable) 1.0, -9);
+		 keyvalues.addValue((Comparable) 2.0, -2);
+		 KeyedValues object_under_test = DataUtilities.getCumulativePercentages((KeyedValues) keyvalues);
+		 
+		 assertEquals(0.0, (double) object_under_test.getValue(0), 0.000000001d);
+	 }
+	 
+	 @Test //expected 0.0
+	 public void testOutofRangeLeftDataCumulativePercentages() {
+		 DefaultKeyedValues keyvalues = new DefaultKeyedValues();
+		 keyvalues.addValue((Comparable) 0.0, Double.MIN_VALUE);
+		 keyvalues.addValue((Comparable) 1.0, Double.MIN_VALUE);
+		 keyvalues.addValue((Comparable) 2.0, Double.MIN_VALUE);
+		 KeyedValues object_under_test = DataUtilities.getCumulativePercentages((KeyedValues) keyvalues);
+		 
+		 assertEquals(0.0, (double) object_under_test.getValue(1), 0.000000001d);
+	 }
+	 
+	 @Test //expected; NaN
+	 public void testOutofRangeRightDataCumulativePercentages() {
+		 DefaultKeyedValues keyvalues = new DefaultKeyedValues();
+		 keyvalues.addValue((Comparable) 0.0, Double.POSITIVE_INFINITY);
+		 keyvalues.addValue((Comparable) 1.0, Double.POSITIVE_INFINITY);
+		 keyvalues.addValue((Comparable) 2.0, Double.POSITIVE_INFINITY);
+		 KeyedValues object_under_test = DataUtilities.getCumulativePercentages((KeyedValues) keyvalues);
+		 
+		 double result = (double) object_under_test.getValue(0);
+		 
+		 if(Double.isNaN(result)) {
+			 assertTrue(true); //pass the test, since 0 / 0 is undefined NaN
+		 } else {
+			 assertEquals(0.0, result, 0.000000001d);
+		 }
+	 }
+	 
+	 @Test //expected NaN
+	 public void testMaximumDataCumulativePercentages() {
+		 DefaultKeyedValues keyvalues = new DefaultKeyedValues();
+		 keyvalues.addValue((Comparable) 0.0, Double.MAX_VALUE);
+		 keyvalues.addValue((Comparable) 1.0, Double.MAX_VALUE);
+		 keyvalues.addValue((Comparable) 2.0, Double.MAX_VALUE);
+		 KeyedValues object_under_test = DataUtilities.getCumulativePercentages((KeyedValues) keyvalues);
+		 
+		 double result = (double) object_under_test.getValue(0);
+		 
+		 if(Double.isNaN(result)) {
+			 assertTrue(true); //pass the test, since 0 / 0 is undefined NaN
+		 } else {
+			 assertEquals(0.0, result, 0.000000001d);
+		 }
+	 }
+	 
+	 @Test //expected: IllegalArgumentException
+	 public void testNullCumulativePercentages() {
+		 try {
+			 DataUtilities.getCumulativePercentages(null);
+	         fail("Expected IllegalArgumentException");
+	     } catch (IllegalArgumentException e) {
+	    	 // This is the expected behavior, so the test passes.
+	     }
+	 }
+	  
+	 
+}
--- Lab2/src/org/jfree/data/test/RangeTest.java
+++ Lab2/src/org/jfree/data/test/RangeTest.java
@@ -39,6 +39,353 @@ public class RangeTest {
 	public void testIntersectsLowerLeftOfRangeUpperLeftBoundaryReturnsTrue() { 
 		// Boundary Assumption
 		assertTrue("Should return true", rangeObjectUnderTest.intersects(1, 5));
+<<<<<<< HEAD
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftOfRangeUpperInRangeReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(1, 7));
+	}
+
+	@Test
+	public void testIntersectsLowerLeftOfRangeUpperRightBoundaryReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(1, 10));
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftOfRangeUpperRightOfRangeReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(1, 12));
+	}
+	
+	@Test
+	public void testIntersectsBothLowerBoundaryReturnsTrue() { 
+		// Boundary Assumption
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(5, 5));
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftBoundaryUpperInRangeReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(5, 7));
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftBoundaryUpperRightBoundaryReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(5, 10));
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftBoundaryUpperRightOfRangeReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(5, 13));
+	}
+	
+	@Test
+	public void testIntersectsBothInRangeReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(7, 9));
+	}
+	
+	@Test
+	public void testIntersectsLowerInRangeUpperRightBoundaryReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(7, 10));
+	}
+	
+	@Test
+	public void testIntersectsLowerInRangeUpperRightOfRangeReturnsTrue() { 
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(7, 13));
+	}
+	
+	@Test
+	public void testIntersectsBothRightBoundaryReturnsTrue() { 
+		// Boundary Assumption
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(10, 10));
+	}
+	
+	@Test
+	public void testIntersectsLowerRightBoundaryUpperRightOfRangeReturnsTrue() { 
+		// Boundary Assumption
+		assertTrue("Should return true", rangeObjectUnderTest.intersects(10, 14));
+	}
+	
+	@Test
+	public void testIntersectsBothRightOfRangeReturnsFalse() { 
+		assertFalse("Should return false", rangeObjectUnderTest.intersects(13, 15));
+	}
+	
+	// with negatives (-10, -5)
+	@Test
+	public void testIntersectsBothLeftOfRangeNegativesReturnsFalse() { 
+		assertFalse("Should return false", negativeRangeObjectUnderTest.intersects(-15, -13));
+	}
+	
+	@Test
+	public void testIntersectsLowerLeftOfRangeUpperInRangeNegativesReturnsTrue() { 
+		assertTrue("Should return true", negativeRangeObjectUnderTest.intersects(-15, -7));
+	}
+	
+	@Test
+	public void testIntersectsBothInRangeNegativesReturnsTrue() { 
+		assertTrue("Should return true", negativeRangeObjectUnderTest.intersects(-9, -6));
+	}
+
+	@Test
+	public void testIntersectsLowerInRangeUpperRightOfRangeNegativesReturnsTrue() { 
+		assertTrue("Should return true", negativeRangeObjectUnderTest.intersects(-9, -3));
+	}
+	
+	@Test
+	public void testIntersectsBothRightOfRangeNegativesReturnsFalse() { 
+		assertFalse("Should return false", negativeRangeObjectUnderTest.intersects(-4, -2));
+	}
+		
+	
+	// Test methods for getLowerBound()
+	
+	@Test
+	public void testLowerBoundTwoNegatives() { 
+		customRangeObjectUnderTest = new Range(-5,-1);
+		assertEquals("The lower bound should be -5",
+				-5, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundOneNegativeOneZero() { 
+		customRangeObjectUnderTest = new Range(-5,0);
+		assertEquals("The lower bound should be -5",
+				-5, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundOneNegativeOnePositive() { 
+		customRangeObjectUnderTest = new Range(-5,1);
+		assertEquals("The lower bound should be -5",
+				-5, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundTwoZeros() { 
+		customRangeObjectUnderTest = new Range(0,0);
+		assertEquals("The lower bound should be 0",
+				0, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundOneZeroOnePositive() { 
+		customRangeObjectUnderTest = new Range(0,5);
+		assertEquals("The lower bound should be 0",
+				0, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundTwoPositives() { 
+		customRangeObjectUnderTest = new Range(5,15);
+		assertEquals("The lower bound should be 5",
+				5, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundEqualPositives() { 
+		customRangeObjectUnderTest = new Range(5,5);
+		assertEquals("The lower bound should be 5",
+				5, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testLowerBoundEqualNegatives() { 
+		customRangeObjectUnderTest = new Range(-5,-5);
+		assertEquals("The lower bound should be -5",
+				-5, customRangeObjectUnderTest.getLowerBound(), 0.000000001d);
+	}
+	
+	
+	// Test methods for getUpperBound()
+	
+	@Test
+	public void testUpperBoundTwoNegatives() { 
+		customRangeObjectUnderTest = new Range(-5,-1);
+		assertEquals("The upper bound should be -1",
+				-1, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundOneNegativeOneZero() { 
+		customRangeObjectUnderTest = new Range(-5,0);
+		assertEquals("The upper bound should be 0",
+				0, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundOneNegativeOnePositive() { 
+		customRangeObjectUnderTest = new Range(-5,1);
+		assertEquals("The upper bound should be 1",
+				1, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundTwoZeros() { 
+		customRangeObjectUnderTest = new Range(0,0);
+		assertEquals("The upper bound should be 0",
+				0, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundOneZeroOnePositive() { 
+		customRangeObjectUnderTest = new Range(0,5);
+		assertEquals("The upper bound should be 5",
+				5, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundTwoPositives() { 
+		customRangeObjectUnderTest = new Range(5,15);
+		assertEquals("The upper bound should be 15",
+				15, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundEqualPositives() { 
+		customRangeObjectUnderTest = new Range(5,5);
+		assertEquals("The upper bound should be 5",
+				5, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	@Test
+	public void testUpperBoundEqualNegatives() { 
+		customRangeObjectUnderTest = new Range(-5,-5);
+		assertEquals("The upper bound should be -5",
+				-5, customRangeObjectUnderTest.getUpperBound(), 0.000000001d);
+	}
+	
+	
+	// Test methods for getCentralValues()
+	
+	@Test
+	public void testCentralValueShouldBeZero() { 
+		customRangeObjectUnderTest = new Range(-1,1);
+		assertEquals("The central value of -1 and 1 should be 0",
+				0, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+	
+	@Test
+	public void testCentralValueEqualValues() { 
+		customRangeObjectUnderTest = new Range(3,3);
+		assertEquals("The central value of 3 and 3 should be 3",
+				3, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+	
+	@Test
+	public void testCentralValueTwoPositives() { 
+		customRangeObjectUnderTest = new Range(3,5);
+		assertEquals("The central value of 3 and 5 should be 4",
+				4, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+	
+	@Test
+	public void testCentralValueNegativeValues() { 
+		customRangeObjectUnderTest = new Range(-5,-3);
+		assertEquals("The central value of -5 and -3 should be -4",
+				-4, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+	
+	@Test
+	public void testCentralValueOneNegativeOneZero() { 
+		customRangeObjectUnderTest = new Range(-5,0);
+		assertEquals("The central value of -5 and 0 should be -2.5",
+				-2.5, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+	
+	@Test
+	public void testCentralValueOneNegativeOnePositive() { 
+		customRangeObjectUnderTest = new Range(-5,2);
+		assertEquals("The central value of -5 and 2 should be -1.5",
+				-1.5, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+	
+	@Test
+	public void testCentralValueBothZero() { 
+		customRangeObjectUnderTest = new Range(0,0);
+		assertEquals("The central value of 0 and 0 should be 0",
+				0, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+	
+	@Test
+	public void testCentralValueOneZeroOnePositive() { 
+		customRangeObjectUnderTest = new Range(0,5);
+		assertEquals("The central value of 0 and 5 should be 2.5",
+				2.5, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+	
+	@Test
+	public void testCentralValueEqualNegativeValues() { 
+		customRangeObjectUnderTest = new Range(-3,-3);
+		assertEquals("The central value of -3 and -3 should be -3",
+				-3, customRangeObjectUnderTest.getCentralValue(), 0.000000001d);
+	}
+	
+	
+	// Test methods for constrain(double value)
+	
+	// Using (5, 10) by default
+	@Test
+	public void testConstrainMatchesLowerRange() { 
+		assertEquals("The constrain value should be 5",
+				5, rangeObjectUnderTest.constrain(4), 0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainMatchesUpperRange() { 
+		assertEquals("The constrain value should be 10",
+				10, rangeObjectUnderTest.constrain(12), 0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainWithinRange() { 
+		assertEquals("Should return input value (7)",
+				7, rangeObjectUnderTest.constrain(7), 0.000000001d);
+	}
+	
+	
+	@Test
+	public void testConstrainLowerBoundary() { 
+		assertEquals("The constrain value should be 5",
+				5, rangeObjectUnderTest.constrain(5), 0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainUpperBoundary() { 
+		assertEquals("The constrain value should be 10",
+				10, rangeObjectUnderTest.constrain(10), 0.000000001d);
+	}
+	
+	// Negatives using (-10, -5)
+	@Test
+	public void testConstrainMatchesLowerRangeNegative() { 
+		assertEquals("The constrain value should be -10",
+				-10, negativeRangeObjectUnderTest.constrain(-11), 0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainMatchesUpperRangeNegative() { 
+		assertEquals("The constrain value should be -5",
+				-5, negativeRangeObjectUnderTest.constrain(-4), 0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainWithinRangeNegative() { 
+		assertEquals("Should return input value (-7)",
+				-7, negativeRangeObjectUnderTest.constrain(-7), 0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainNegativeLowerBoundary() { 
+		assertEquals("The constrain value should be -10",
+				-10, negativeRangeObjectUnderTest.constrain(-10), 0.000000001d);
+	}
+	
+	@Test
+	public void testConstrainNegativeUpperBoundary() { 
+		assertEquals("The constrain value should be -5",
+				-5, negativeRangeObjectUnderTest.constrain(-5), 0.000000001d);
+=======
+>>>>>>> 6ad9728 (updating old files)
 	}
 	
 	@Test
@@ -384,4 +731,5 @@ public class RangeTest {
 		assertEquals("The constrain value should be -5",
 				-5, negativeRangeObjectUnderTest.constrain(-5), 0.000000001d);
 	}
+	
 }
